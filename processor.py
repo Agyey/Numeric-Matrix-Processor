795056646ef5551a61ee6096ec28a584e8ea3d7f
Agyey Arya <agyey27@yahoo.co.in>
2020-12-11 21:27:52 +0530

    4 Directional Transpose

--- processor.py
+++ processor.py
@@ -35,6 +35,38 @@ class Matrix:
     def empty_matrix(cls, x, y):
         return cls(x, y, True)
 
+    @property
+    def main_transpose(self):
+        matR = Matrix(self.y, self.x)
+        for i in range(matR.x):
+            for j in range(matR.y):
+                matR.__matrix__[i][j] = self.__matrix__[j][i]
+        return matR
+
+    @property
+    def side_transpose(self):
+        matR = Matrix(self.y, self.x)
+        for i in range(matR.x):
+            for j in range(matR.y):
+                matR.__matrix__[i][j] = self.__matrix__[self.y-j-1][self.x-i-1]
+        return matR
+
+    @property
+    def vertical_transpose(self):
+        matR = Matrix(self.x, self.y)
+        for i in range(matR.x):
+            for j in range(matR.y):
+                matR.__matrix__[i][j] = self.__matrix__[i][self.y-j-1]
+        return matR
+
+    @property
+    def horizontal_transpose(self):
+        matR = Matrix(self.x, self.y)
+        for i in range(matR.x):
+            for j in range(matR.y):
+                matR.__matrix__[i][j] = self.__matrix__[self.x-i-1][j]
+        return matR
+
     def get_matrix(self):
         for i in range(self.x):
             try:
@@ -97,22 +129,26 @@ class Menu:
         print(f'Enter a Number Between 0-{len(self.options)}')
 
     def start(self):
+        for index, option in enumerate(self.options):
+            print(f'{index + 1}. {option}')
+        print(f'0. {self.exit_phrase}')
+        try:
+            choice = int(input(self.input_phrase))
+            if 0 < choice <= len(self.options):
+                choice = list(self.options.keys())[choice - 1]
+                self.options[choice]()
+            elif choice == 0:
+                self.running = False
+            else:
+                self.invalid_choice()
+        except ValueError:
+            self.invalid_choice()
+
+    def loop(self):
         self.running = True
         while self.running:
-            for index, option in enumerate(self.options):
-                print(f'{index+1}. {option}')
-            print(f'0. {self.exit_phrase}')
-            try:
-                choice = int(input(self.input_phrase))
-                if 0 < choice <= len(self.options):
-                    choice = list(self.options.keys())[choice-1]
-                    self.options[choice]()
-                elif choice == 0:
-                    self.running = False
-                else:
-                    self.invalid_choice()
-            except ValueError:
-                self.invalid_choice()
+            self.start()
+
 
 
 def add_matrix():
@@ -152,16 +188,60 @@ def matrix_multiplication():
     print()
 
 
+def matrix_transpose():
+    # Default Transpose Type
+    print()
+    global transpose_type
+
+    def main_transpose():
+        global transpose_type
+        transpose_type = 'main'
+
+    def side_transpose():
+        global transpose_type
+        transpose_type = 'side'
+
+    def vertical_transpose():
+        global transpose_type
+        transpose_type = 'vertical'
+
+    def horizontal_transpose():
+        global transpose_type
+        transpose_type = 'horizontal'
+
+    transpose_options = {
+        'Main diagonal': main_transpose,
+        'Side diagonal': side_transpose,
+        'Vertical line': vertical_transpose,
+        'Horizontal line': horizontal_transpose,
+    }
+    input_phrase = 'Your choice: '
+    exit_phrase = 'Exit'
+    transpose_menu = Menu(input_phrase, exit_phrase, transpose_options)
+    try:
+        transpose_menu.start()
+    except (MatrixDimensionMismatch, ValueError, InsufficientInformation, EmptyMatrix, IllegalData):
+        print('ERROR')
+    matA = Matrix(*map(int, input('Enter size of matrix: ').split()))
+    print('Enter matrix:')
+    matA.get_matrix()
+    print(f"{transpose_type}_transpose")
+    print('The result is')
+    print(getattr(matA, f"{transpose_type}_transpose"))
+    print()
+
+
 options = {
     'Add matrices': add_matrix,
     'Multiply matrix by a constant': scalar_multiplication,
     'Multiply matrices': matrix_multiplication,
+    'Transpose matrix': matrix_transpose,
 }
 input_phrase = 'Your choice: '
 exit_phrase = 'Exit'
-
+transpose_type = 'main'
 menu = Menu(input_phrase, exit_phrase, options)
 try:
-    menu.start()
+    menu.loop()
 except (MatrixDimensionMismatch, ValueError, InsufficientInformation, EmptyMatrix, IllegalData):
     print('ERROR')
